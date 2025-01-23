09ee62cd41d09b1a56698da6781f4516f50cb856
GRB <113555208+GiovannaRb@users.noreply.github.com>
2025-01-23 11:01:28 -0300

    Variavel _repository

---
+++ InjecaoDeDependencia - API/Injeção de dependência/Controllers/IDController.cs
@@ -0,0 +1,37 @@
+using Injeção_de_dependência.Interfaces;
+using Injeção_de_dependência.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
+
+namespace Injeção_de_dependência.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IDController : ControllerBase
+    {
+        public readonly IDInterface _repository;
+
+        public IDController(IDInterface repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public void Cadastrar(dadosModel dados)
+        {
+            _repository.Cadastrar(dados);
+        }
+
+        [HttpPost]
+        public void Editar(dadosModel dados)
+        {
+            _repository.Editar(dados);
+        }
+
+        [HttpDelete]
+        public void Deletar(int id)
+        {
+            _repository.Deletar(id);
+        }
+    }
+}
---
+++ InjecaoDeDependencia - API/Injeção de dependência/Models/dadosModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Injeção_de_dependência.Models
+{
+    public class dadosModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
