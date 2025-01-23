84839ac716351ff6486f797053ca5754b5d2266a
GRB <113555208+GiovannaRb@users.noreply.github.com>
2025-01-23 11:02:48 -0300

    Delete InjecaoDeDependencia directory

--- InjecaoDeDependencia/Injeção de dependência/Models/dadosModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Injeção_de_dependência.Models
-{
-    public class dadosModel
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
